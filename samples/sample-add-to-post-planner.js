2b41474dd4f8f031c9963d650fc558c78133683a
Unknown <scott.bowler@dcsworldwide.net>
2019-08-01 13:15:50 +0100

    Modify samples to include ""

--- samples/sample-add-to-post-planner.js
+++ samples/sample-add-to-post-planner.js
@@ -1,43 +1,43 @@
 module.exports =   {
-    categories: [
+    "categories": [
       {
-        id: '7798ab2f-b1c6-48c0-b043-219b24ebf60a',
-        name: 'Demo Category',
-        max_count: 0,
-        order: {
-            draft: [
+        "id": '7798ab2f-b1c6-48c0-b043-219b24ebf60a',
+        "name": 'Demo Category',
+        "max_count": 0,
+        "order": {
+            "draft": [
                 '029f1d0a-e2b9-41c8-b883-ca8f0d28a494'
             ],
-            pending: [],
-            approved: [],
-            archived: []
+            "pending": [],
+            "approved": [],
+            "archived": []
         },
-        color: '#4E342E',
-        user_id: 4,
-        team_id: 15,
-        created_at: '2018-02-21 12:50:46',
-        updated_at: '2018-05-11 06:58:46',
-        post_status: 'draft'
+        "color": '#4E342E',
+        "user_id": 4,
+        "team_id": 15,
+        "created_at": '2018-02-21 12:50:46',
+        "updated_at": '2018-05-11 06:58:46',
+        "post_status": 'draft'
       }
     ],
-    evergreen_posts: [
+    "evergreen_posts": [
         {
-            category_id: '7798952f-b1c6-48c0-b043-219b24ebf60a',
-            data: {
-                content_is_remote_text: false,
-                text: 'Demo content',
-                media_id: '1fef36d4-f7b5-4449-94d4-7fc2302c657f',
-                media_url: 'https://holly.nyc3.digitaloceanspaces.com/_test-files/logo-square-1024.png',
-                media_thumbnail: 'https://holly.nyc3.digitaloceanspaces.com/_test-files/logo-square-1024.png'
+            "category_id": '7798952f-b1c6-48c0-b043-219b24ebf60a',
+            "data": {
+                "content_is_remote_text": false,
+                "text": 'Demo content',
+                "media_id": '1fef36d4-f7b5-4449-94d4-7fc2302c657f',
+                "media_url": 'https://holly.nyc3.digitaloceanspaces.com/_test-files/logo-square-1024.png',
+                "media_thumbnail": 'https://holly.nyc3.digitaloceanspaces.com/_test-files/logo-square-1024.png'
             },
-            type: 'image',
-            user_id: 1,
-            team_id: 1,
-            status: 'draft',
-            max_count: 0,
-            id: '029f1d0a-e2b9-41c8-b883-ca8f0d28a494',
-            updated_at: '2018-05-11 06:58:46',
-            created_at: '2018-05-11 06:58:46'
+            "type": 'image',
+            "user_id": 1,
+            "team_id": 1,
+            "status": 'draft',
+            "max_count": 0,
+            "id": '029f1d0a-e2b9-41c8-b883-ca8f0d28a494',
+            "updated_at": '2018-05-11 06:58:46',
+            "created_at": '2018-05-11 06:58:46'
         }
     ]
 }
--- samples/sample-upload-media.js
+++ samples/sample-upload-media.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'https://holly.nyc3.digitaloceanspaces.com/_test-files/logo-square-1024.png',
-    id: '6333cd79-0796-411d-868c-571d7df00b21',
+    "url": 'https://holly.nyc3.digitaloceanspaces.com/_test-files/logo-square-1024.png',
+    "id": '6333cd79-0796-411d-868c-571d7df00b21',
 }
